52843516eb1ad01fb5df658efe7685e2ba8ada94
Yunf.li37@gmail.com <Yunf.li37@gmail.com>
2017-09-16 15:52:25 -0400

    updated image/rotate

--- Player.py
+++ Player.py
@@ -3,8 +3,9 @@ from pygame.sprite import Sprite
 class Player(Sprite):
 	def __init__(self, image,start_x, start_y, screen):
 		super(Player,self).__init__(); #because it's a subclass, you have to call the parent class Sprite
-		self.image = pygame.image.load(image);
-		#can scale the image with pygame.scale(self.image (200, 200));
+		self.image_before = pygame.image.load(image);
+		#can scale the image with 
+		self.image = pygame.transform.scale(self.image_before,(100, 150));
 		self.x = start_x;
 		self.y = start_y;
 		self.speed = 10;
@@ -34,3 +35,14 @@ class Player(Sprite):
 			self.should_move_left = yes_or_no;
 		if(direction =="right"):
 			self.should_move_right = yes_or_no;
+	def transform_image(self):
+		if(self.should_move_up):
+			self.y -= self.speed;
+		elif(self.should_move_down):
+			self.y += self.speed;
+		if(self.should_move_left):
+			self.x -= self.speed;
+		elif(self.should_move_right):
+			self.x += self.speed;
+		self.image = pygame.transform.flip(self.image, True, False);
+		self.screen.blit(self.image, [self.x, self.y])
--- game.py
+++ game.py
@@ -16,7 +16,7 @@ black = (0,0,0)
 background_color = (82, 111, 53) # it's because we are going to pain abckground , we need a tuple
 screen = pygame.display.set_mode(screen_size);
 pygame.display.set_caption("Hi");
-the_player = Player("./images/tidus.jpg", 800, 100, screen) # dont need the image, the x or the y
+the_player = Player("./images/ff1.tiff", 800, 100, screen) # dont need the image, the x or the y
 bad_guy = Bad_Guy(screen);
 bullets = Group(); #make a new group called bullets, it's a pygame "list";
 
@@ -29,13 +29,21 @@ def message_display(text): #display what kind of text you want
 	start_text = largeText.render(text, True, (0,0,0));
 	screen.blit(start_text, [100, 100]);
 	pygame.display.update()
+def message_players_name(text, name):
+	largeText = pygame.font.Font(None ,70)
+	story = largeText.render(text, True, (0,0,0));
+	start_text = largeText.render(name, True, (0,0,0));
+	screen.blit(start_text, [screenx/2 -125, screeny/2]);
+	screen.blit(story, [screenx/2 -350, screeny/2 -50]);
+	pygame.display.update()
+
 
 def game_loop():
 	starting_text = True;
 	game_on = True;
 	tick = 0;
 	while game_on: #will run forever until break
-		tick+=1;
+		tick+=1; #keeps track of seconds/frames per second
 		for event in pygame.event.get(): #loop through all the pygame events, gave it a escape patch
 			if event.type == pygame.QUIT:
 				game_on = False;
@@ -48,6 +56,7 @@ def game_loop():
 					the_player.should_move("right", True);
 				elif(event.key == 276):
 					the_player.should_move("left", True);
+					the_player.transform_image();
 				elif(event.key == 32):
 					new_bullet = Bullet(screen, the_player);
 					bullets.add(new_bullet);
@@ -60,18 +69,27 @@ def game_loop():
 					the_player.should_move("right", False);
 				elif(event.key == 276):
 					the_player.should_move("left", False);
+					the_player.transform_image();
 		screen.fill(background_color); #pain the screen, no need for blit because we are not drawing over another picture
 		if(starting_text == True): #added welcome player logo at the start of the game.
 			message_display("WELCOME PLAYER");
+			message_players_name("Welcome Brave Adventurer", "Zack")
 			if(tick % 30 == 0):	
 				starting_text = False;
+
 		bad_guy.update_me(the_player)
 		bad_guy.draw_me();
-		the_player.draw_me(); # this method draws himself in it's own method instead of bliting on the main game screen
 
+		#================================================================================
+			#checking if the player is left or right side, flip the image accordingly
+
+		#if(the_player.x > screenx/2):
+		the_player.draw_me(); # this method draws himself in it's own method instead of bliting on the main game screen
+		#elif(the_player.x <= screenx/2):
 		for bullet in bullets:
 			bullet.update()
 			bullet.draw_bullet();
+
 
 
 		pygame.display.flip(); #flip the screen so we can draw again and again
